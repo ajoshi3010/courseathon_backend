cd4db7fb8fffe9e9723e328efa3ac7df32e4d361
Anirudh Joshi <vuddawaranirudhjoshi03@gmail.com>
2024-03-19 19:47:57 +0530

    removed jwt so i can facilitate clerk in frontend

--- models/Module.js
+++ models/Module.js
@@ -5,10 +5,10 @@ const Schema = mongoose.Schema;
 
 const moduleSchema = new Schema({
   title: String,
-  videoLink: String,
-  meetingLink: String,
+  link: String, // Single link field
   meetingDate: Date, // Field for meeting date
   uploadedDate: { type: Date, default: Date.now }, // Field for uploaded date, defaults to current date
+  isLive: { type: Number, enum: [0, 1], default: 0 } // Field for live status, defaults to 0
   // Add more fields as needed
 });
 
--- models/Student.js
+++ models/Student.js
@@ -10,8 +10,7 @@ const studentSchema = new Schema({
     required: true,
     unique: true
   },
-  password: String, // Or any other credential field you may need
-  // Add more fields as needed
+
 });
 
 module.exports = mongoose.model('Student', studentSchema);
